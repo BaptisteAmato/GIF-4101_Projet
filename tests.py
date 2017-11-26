b52f20ccd2c0a43216783fb5861174094a1e0d28
Baptiste Amato <baptiste.amato@gmail.com>
2017-11-26 17:31:34 -0500

    Advances.

--- tests.py
+++ tests.py
@@ -4,204 +4,288 @@ from keras.callbacks import ModelCheckpoint
 from keras.layers import Activation, ZeroPadding2D, BatchNormalization, Conv2D, UpSampling2D
 from keras.layers import MaxPooling2D, Dropout
 from keras.models import Sequential
-
+from utils import *
 K.set_image_data_format('channels_last')
 
-from utils import *
+
+# def MyModel(input_shape):
+#     X_input = Input(input_shape)
+#
+#     ############# ENCODER ###############
+#     # 224x224x1
+#     #     X = ZeroPadding2D((1, 1))(X_input)
+#     X = Conv2D(64, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv0')(X_input)
+#     #     X = BatchNormalization(axis = 3, name = 'bn0')(X)
+#     #     X = Activation('relu')(X)
+#
+#
+#     # 224x224x64
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(64, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv1')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn1')(X)
+#     #     X = Activation('relu')(X)
+#     # 224x224x64
+#     X = MaxPooling2D((2, 2), strides=(2, 2), name='max_pool1')(X)
+#
+#     # 112x112x64
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv2')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn2')(X)
+#     #     X = Activation('relu')(X)
+#
+#     # 112x112x128
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv3')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn3')(X)
+#     #     X = Activation('relu')(X)
+#     # 112x112x128
+#     X = MaxPooling2D((2, 2), strides=(2, 2), name='max_pool3')(X)
+#
+#     # 56x56x128
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv4')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn4')(X)
+#     #     X = Activation('relu')(X)
+#
+#     # 56x56x256
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv5')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn5')(X)
+#     #     X = Activation('relu')(X)
+#
+#     # 56x56x256
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv6')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn6')(X)
+#     #     X = Activation('relu')(X)
+#     # 56x56x256
+#     X = MaxPooling2D((2, 2), strides=(2, 2), name='max_pool6')(X)
+#
+#     # 28x28x256
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv7')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn7')(X)
+#     #     X = Activation('relu')(X)
+#
+#     # 28x28x512
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv8')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn8')(X)
+#     #     X = Activation('relu')(X)
+#
+#     # 28x28x512
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv9')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn9')(X)
+#     #     X = Activation('relu')(X)
+#     # 28x28x512
+#     X = MaxPooling2D((2, 2), strides=(2, 2), name='max_pool9')(X)
+#
+#     # 14x14x512
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv10')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn10')(X)
+#     #     X = Activation('relu')(X)
+#
+#     # 14x14x512
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv11')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn11')(X)
+#     #     X = Activation('relu')(X)
+#
+#     # 14x14x512
+#     #     X = ZeroPadding2D((1, 1))(X)
+#     X = Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv12')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn12')(X)
+#     #     X = Activation('relu')(X)
+#     # 14x14x512
+#     X = MaxPooling2D((2, 2), strides=(2, 2), name='max_pool12')(X)
+#
+#     # 7x7x512
+#     print("--")
+#     print(X.shape)
+#
+#     ############# DECODER ###############
+#     # 7x7x512
+#     X = Conv2D(512, (1, 1), strides=(1, 1), activation='relu', name='conv13')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn13')(X)
+#     #     X = Activation('relu')(X)
+#
+#     # 7x7x512
+#     X = UpSampling2D(size=(2, 2), name='upsampling14')(X)
+#     #     X = ZeroPadding2D((2, 2))(X)
+#     X = Conv2D(512, (5, 5), strides=(1, 1), padding='same', activation='relu', name='conv14')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn14')(X)
+#     #     X = Activation('relu')(X)
+#
+#     X = UpSampling2D(size=(2, 2), name='upsampling15')(X)
+#     #     X = ZeroPadding2D((2, 2))(X)
+#     X = Conv2D(256, (5, 5), strides=(1, 1), padding='same', activation='relu', name='conv15')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn15')(X)
+#     #     X = Activation('relu')(X)
+#
+#     X = UpSampling2D(size=(2, 2), name='upsampling16')(X)
+#     #     X = ZeroPadding2D((2, 2))(X)
+#     X = Conv2D(128, (5, 5), strides=(1, 1), padding='same', activation='relu', name='conv16')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn16')(X)
+#     #     X = Activation('relu')(X)
+#
+#     X = UpSampling2D(size=(2, 2), name='upsampling17')(X)
+#     #     X = ZeroPadding2D((2, 2))(X)
+#     X = Conv2D(64, (5, 5), strides=(1, 1), padding='same', activation='relu', name='conv17')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn17')(X)
+#     #     X = Activation('relu')(X)
+#
+#     X = UpSampling2D(size=(2, 2), name='upsampling18')(X)
+#     #     X = ZeroPadding2D((2, 2))(X)
+#     X = Conv2D(32, (5, 5), strides=(1, 1), padding='same', activation='relu', name='conv18')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn18')(X)
+#     #     X = Activation('relu')(X)
+#
+#     #     X = ZeroPadding2D((2, 2))(X)
+#     X = Conv2D(1, (5, 5), strides=(1, 1), padding='same', activation='sigmoid', name='conv19')(X)
+#     #     X = BatchNormalization(axis = 3, name = 'bn19')(X)
+#     #     X = Activation('sigmoid')(X)
+#
+#     # 224x224x1
+#     print("--")
+#     print(X.shape)
+#
+#     model = Model(inputs=X_input, outputs=X, name='MyModel')
+#
+#     return model
 
 
-def MyModel(input_shape):
+# TODO: use Conv2DTranspose
+def MyModel(input_shape, dropout=0.3):
     model = Sequential([
         ############# ENCODER ###############
         # 224x224x1
-        ZeroPadding2D((1, 1), input_shape=input_shape),
-        Conv2D(64, (3, 3), strides=(1, 1), name='conv0'),
-        BatchNormalization(axis=3, name='bn0'),
-        Activation('relu'),
-
+        Conv2D(64, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv0', input_shape=input_shape),
         # 224x224x64
-        ZeroPadding2D((1, 1)),
-        Conv2D(64, (3, 3), strides=(1, 1), name='conv1'),
-        BatchNormalization(axis=3, name='bn1'),
-        Activation('relu'),
+        Conv2D(64, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv1'),
         # 224x224x64
         MaxPooling2D((2, 2), strides=(2, 2), name='max_pool1'),
-
         # 112x112x64
-        ZeroPadding2D((1, 1)),
-        Conv2D(128, (3, 3), strides=(1, 1), name='conv2'),
-        BatchNormalization(axis=3, name='bn2'),
-        Activation('relu'),
-
+        Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv2'),
         # 112x112x128
-        ZeroPadding2D((1, 1)),
-        Conv2D(128, (3, 3), strides=(1, 1), name='conv3'),
-        BatchNormalization(axis=3, name='bn3'),
-        Activation('relu'),
+        Conv2D(128, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv3'),
+        # 112x112x128
         MaxPooling2D((2, 2), strides=(2, 2), name='max_pool3'),
-
         # 56x56x128
-        ZeroPadding2D((1, 1)),
-        Conv2D(256, (3, 3), strides=(1, 1), name='conv4'),
-        BatchNormalization(axis=3, name='bn4'),
-        Activation('relu'),
-
+        Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv4'),
         # 56x56x256
-        ZeroPadding2D((1, 1)),
-        Conv2D(256, (3, 3), strides=(1, 1), name='conv5'),
-        BatchNormalization(axis=3, name='bn5'),
-        Activation('relu'),
-
+        Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv5'),
         # 56x56x256
-        ZeroPadding2D((1, 1)),
-        Conv2D(256, (3, 3), strides=(1, 1), name='conv6'),
-        BatchNormalization(axis=3, name='bn6'),
-        Activation('relu'),
+        Conv2D(256, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv6'),
         # 56x56x256
         MaxPooling2D((2, 2), strides=(2, 2), name='max_pool6'),
-
         # 28x28x256
-        ZeroPadding2D((1, 1)),
-        Conv2D(512, (3, 3), strides=(1, 1), name='conv7'),
-        BatchNormalization(axis=3, name='bn7'),
-        Activation('relu'),
-
+        Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv7'),
         # 28x28x512
-        ZeroPadding2D((1, 1)),
-        Conv2D(512, (3, 3), strides=(1, 1), name='conv8'),
-        BatchNormalization(axis=3, name='bn8'),
-        Activation('relu'),
-
+        Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv8'),
         # 28x28x512
-        ZeroPadding2D((1, 1)),
-        Conv2D(512, (3, 3), strides=(1, 1), name='conv9'),
-        BatchNormalization(axis=3, name='bn9'),
-        Activation('relu'),
+        Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv9'),
         # 28x28x512
         MaxPooling2D((2, 2), strides=(2, 2), name='max_pool9'),
-
         # 14x14x512
-        ZeroPadding2D((1, 1)),
-        Conv2D(512, (3, 3), strides=(1, 1), name='conv10'),
-        BatchNormalization(axis=3, name='bn10'),
-        Activation('relu'),
-
+        Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv10'),
         # 14x14x512
-        ZeroPadding2D((1, 1)),
-        Conv2D(512, (3, 3), strides=(1, 1), name='conv11'),
-        BatchNormalization(axis=3, name='bn11'),
-        Activation('relu'),
-
+        Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv11'),
         # 14x14x512
-        ZeroPadding2D((1, 1)),
-        Conv2D(512, (3, 3), strides=(1, 1), name='conv12'),
-        BatchNormalization(axis=3, name='bn12'),
-        Activation('relu'),
+        Conv2D(512, (3, 3), strides=(1, 1), padding='same', activation='relu', name='conv12'),
         # 14x14x512
         MaxPooling2D((2, 2), strides=(2, 2), name='max_pool12'),
 
         ############# DECODER ###############
         # 7x7x512
-        Conv2D(512, (1, 1), strides=(1, 1), name='conv13'),
-        BatchNormalization(axis=3, name='bn13'),
-        Activation('relu'),
-        Dropout(0.5),
-
+        Conv2D(512, (1, 1), strides=(1, 1), activation='relu', name='conv13'),
+        Dropout(dropout),
         # 7x7x512
         UpSampling2D(size=(2, 2), name='upsampling14'),
-        ZeroPadding2D((2, 2)),
-        Conv2D(512, (5, 5), strides=(1, 1), name='conv14'),
-        BatchNormalization(axis=3, name='bn14'),
-        Activation('relu'),
-        Dropout(0.5),
-
+        # 14x14x512
+        Conv2D(512, (5, 5), strides=(1, 1), padding='same', activation='relu', name='conv14'),
+        Dropout(dropout),
+        # 14x14x512
         UpSampling2D(size=(2, 2), name='upsampling15'),
-        ZeroPadding2D((2, 2)),
-        Conv2D(256, (5, 5), strides=(1, 1), name='conv15'),
-        BatchNormalization(axis=3, name='bn15'),
-        Activation('relu'),
-        Dropout(0.5),
-
+        # 28x28x512
+        Conv2D(256, (5, 5), strides=(1, 1), padding='same', activation='relu', name='conv15'),
+        Dropout(dropout),
+        # 28x28x256
         UpSampling2D(size=(2, 2), name='upsampling16'),
-        ZeroPadding2D((2, 2)),
-        Conv2D(128, (5, 5), strides=(1, 1), name='conv16'),
-        BatchNormalization(axis=3, name='bn16'),
-        Activation('relu'),
-        Dropout(0.5),
-
+        # 56x56x256
+        Conv2D(128, (5, 5), strides=(1, 1), padding='same', activation='relu', name='conv16'),
+        Dropout(dropout),
+        # 56x56x128
         UpSampling2D(size=(2, 2), name='upsampling17'),
-        ZeroPadding2D((2, 2)),
-        Conv2D(64, (5, 5), strides=(1, 1), name='conv17'),
-        BatchNormalization(axis=3, name='bn17'),
-        Activation('relu'),
-        Dropout(0.5),
-
+        # 112x112x128
+        Conv2D(64, (5, 5), strides=(1, 1), padding='same', activation='relu', name='conv17'),
+        Dropout(dropout),
+        # 112x112x64
         UpSampling2D(size=(2, 2), name='upsampling18'),
-        ZeroPadding2D((2, 2)),
-        Conv2D(32, (5, 5), strides=(1, 1), name='conv18'),
-        BatchNormalization(axis=3, name='bn18'),
-        Activation('relu'),
-        Dropout(0.5),
-
-        ZeroPadding2D((2, 2)),
-        Conv2D(1, (5, 5), strides=(1, 1), name='conv19'),
-        BatchNormalization(axis=3, name='bn19'),
-        Activation('sigmoid')],
+        # 224x224x64
+        Conv2D(32, (5, 5), strides=(1, 1), padding='same', activation='relu', name='conv18'),
+        Dropout(dropout),
+        # 224x224x32
+        Conv2D(1, (5, 5), strides=(1, 1), padding='same', activation='sigmoid', name='conv19'),
+        # 224x224x1
+        ],
         name='MyModel')
-
-    # 224x224x1
 
     return model
 
 
-# Load dataset.
-nb_examples = 2
-crop_size = 224
-X_train, Y_train, X_test, Y_test = load_dataset(nb_examples, crop_size)
-print ("number of training examples = " + str(X_train.shape[0]))
-print ("number of test examples = " + str(X_test.shape[0]))
-print ("X_train shape: " + str(X_train.shape))
-print ("Y_train shape: " + str(Y_train.shape))
-print ("X_test shape: " + str(X_test.shape))
-print ("Y_test shape: " + str(Y_test.shape))
+if __name__ == '__main__':
 
-# Load model. Best weights are saved after each epoch.
-myModel = MyModel(X_train.shape[1:])
-myModel.compile(optimizer="adam", loss='binary_crossentropy', metrics=["accuracy"])
-checkpointer = ModelCheckpoint(filepath="weights.hdf5", verbose=1, save_best_only=True)
-# Run the model.
-hist = myModel.fit(x=X_train, y=Y_train, validation_split=0.3, epochs=10, batch_size=50, callbacks=[checkpointer])
-# Load the best weights (maybe over-fitting after some epochs).
-myModel.load_weights('weights.hdf5')
+    # Load dataset.
+    nb_examples = 2
+    crop_size = 224
+    X_train, Y_train, X_test, Y_test = load_dataset(nb_examples, crop_size)
+    print ("number of training examples = " + str(X_train.shape[0]))
+    print ("number of test examples = " + str(X_test.shape[0]))
+    print ("X_train shape: " + str(X_train.shape))
+    print ("Y_train shape: " + str(Y_train.shape))
+    print ("X_test shape: " + str(X_test.shape))
+    print ("Y_test shape: " + str(Y_test.shape))
 
-# Evaluation of the model.
-preds = myModel.evaluate(x=X_test, y=Y_test)
-print()
-print("Loss = " + str(preds[0]))
-print("Test Accuracy = " + str(preds[1]))
-
-# Test on image: actin + dendrite (no axon for now).
-index = 0
-x = np.load(folder_images_saving_train_x + '/' + str(index) + '.npy')
-y = np.load(folder_images_saving_train_x + '/' + str(index) + '.npy')
-crops_x, crops_y = get_all_crops(x, y, crop_size)
-crop_index = 0
-x = crops_x[crop_index]
-y = crops_y[crop_index]
-img_x, _, img_y = get_image_from_contour_map(x, None, y)
-print()
-test = np.zeros((1, x.shape[0], x.shape[1], x.shape[2]))
-test[0] = x
-
-prediction = myModel.predict(test)
-prediction[prediction <= 0.5] = 0
-prediction[prediction > 0.5] = 1
-
-# Input
-plt.imshow(img_x)
-# Prediction
-plt.figure()
-plt.imshow(get_image_from_contour_map(prediction[0], 'r'))
-# Truth
-plt.figure()
-plt.imshow(img_y)
+    # Load model. Best weights are saved after each epoch.
+    myModel = MyModel(X_train.shape[1:])
+    myModel.compile(optimizer="adam", loss='binary_crossentropy', metrics=["accuracy"])
+    checkpointer = ModelCheckpoint(filepath="weights.hdf5", verbose=1, save_best_only=True)
+    #
+    # # Run the model.
+    # hist = myModel.fit(x=X_train, y=Y_train, validation_split=0.3, epochs=10, batch_size=50, callbacks=[checkpointer])
+    # # Load the best weights (maybe over-fitting after some epochs).
+    # myModel.load_weights('weights.hdf5')
+    #
+    # # Evaluation of the model.
+    # preds = myModel.evaluate(x=X_test, y=Y_test)
+    # print()
+    # print("Loss = " + str(preds[0]))
+    # print("Test Accuracy = " + str(preds[1]))
+    #
+    # # Test on image: actin + dendrite (no axon for now).
+    # index = 0
+    # x = np.load(folder_images_saving_train_x + '/' + str(index) + '.npy')
+    # y = np.load(folder_images_saving_train_x + '/' + str(index) + '.npy')
+    # crops_x, crops_y = get_all_crops(x, y, crop_size)
+    # crop_index = 0
+    # x = crops_x[crop_index]
+    # y = crops_y[crop_index]
+    # img_x, _, img_y = get_image_from_contour_map(x, None, y)
+    # print()
+    # test = np.zeros((1, x.shape[0], x.shape[1], x.shape[2]))
+    # test[0] = x
+    #
+    # prediction = myModel.predict(test)
+    # prediction[prediction <= 0.5] = 0
+    # prediction[prediction > 0.5] = 1
+    #
+    # # Input
+    # plt.imshow(img_x)
+    # # Prediction
+    # plt.figure()
+    # plt.imshow(get_image_from_contour_map(prediction[0], 'r'))
+    # # Truth
+    # plt.figure()
+    # plt.imshow(img_y)
--- utils.py
+++ utils.py
@@ -7,15 +7,16 @@ import skimage.color as color
 import math
 from config import *
 
-
 folder_images_saving = main_folder_path + '/dataset'
 folder_images_saving_train_x = folder_images_saving + '/train_x'
 folder_images_saving_train_y = folder_images_saving + '/train_y'
+
+
 # Image example: 2017-11-14 EXP211 Stim KN93/05_KCl_SMI31-STAR580_MAP2-STAR488_PhSTAR635_1.msr_STED640_Conf561_Conf488_merged.tif
 
 
 def split_tif_image(image):
-        return image[0], image[1], image[2]
+    return image[0], image[1], image[2]
 
 
 # def resize_image(image, width=1024, height=1024):
@@ -29,7 +30,7 @@ def get_smallest_image_dimension():
     min_cols = np.inf
     i = 0
     for file_path in get_files_path_generator():
-        print(str(i+1) + "/" + str(N))
+        print(str(i + 1) + "/" + str(N))
         image = tifffile.imread(file_path)
         # Remove number of channels from shape
         shape = image.shape[1:]
@@ -47,8 +48,8 @@ def get_random_crops(x, y, nb_crops=4, crop_size=224):
     for i in range(0, nb_crops):
         row = np.random.randint(0, x.shape[0] - crop_size)
         col = np.random.randint(0, x.shape[1] - crop_size)
-        crops_x[i] = x[row:row+crop_size, col:col+crop_size]
-        crops_y[i] = y[row:row+crop_size, col:col+crop_size]
+        crops_x[i] = x[row:row + crop_size, col:col + crop_size]
+        crops_y[i] = y[row:row + crop_size, col:col + crop_size]
     return crops_x, crops_y
 
 
@@ -302,7 +303,14 @@ def load_dataset_random_crops(nb_examples=100, nb_crops=4, input_shape=224):
     return train_set_x_orig, train_set_y_orig, train_set_x_orig, train_set_y_orig
 
 
-def load_dataset(nb_examples=100, crop_size=224):
+def load_dataset(nb_examples=100, crop_size=224, min_ones_ratio=0.2):
+    """
+    :param nb_examples:
+    :param crop_size:
+    :param min_ones_ratio: ratio of "1" in the entire matrix. Allows not to save empty matrices.
+    :return:
+    """
+    min_ones = crop_size * min_ones_ratio
     train_set_x_orig = []
     train_set_y_orig = []
     for i in range(0, nb_examples):
@@ -313,19 +321,23 @@ def load_dataset(nb_examples=100, crop_size=224):
         crops_x, crops_y = get_all_crops(x, y, crop_size)
         length = crops_x.shape[0]
         for k in range(0, length):
-            train_set_x_orig.append(crops_x[k])
-            train_set_y_orig.append(crops_y[k])
+            # We do not want to keep black crops, so we make sure there is some data in it.
+            if np.sum(crops_x[k]) > min_ones and np.sum(crops_y[k]) > min_ones:
+                train_set_x_orig.append(crops_x[k])
+                train_set_y_orig.append(crops_y[k])
 
     train_set_x_orig = np.array(train_set_x_orig)
     train_set_y_orig = np.array(train_set_y_orig)
 
     return train_set_x_orig, train_set_y_orig, train_set_x_orig, train_set_y_orig
 
+
 if __name__ == '__main__':
     # display_images_one_by_one()
-    save_train_test_images(100)
+    save_train_test_images(1000)
     # get_smallest_image_dimension()
     # print_images_size()
+    # load_dataset()
 
     # image = tifffile.imread(
     #     '/media/maewanto/B498-74ED/Data_projet_apprentissage/2017-11-14 EXP211 Stim KN93/05_KCl_SMI31-STAR580_MAP2-STAR488_PhSTAR635_1.msr_STED640_Conf561_Conf488_merged.tif')
@@ -351,4 +363,3 @@ if __name__ == '__main__':
     #     '/media/maewanto/B498-74ED/Data_projet_apprentissage/2017-11-14 EXP211 Stim KN93/05_KCl_SMI31-STAR580_MAP2-STAR488_PhSTAR635_1.msr_STED640_Conf561_Conf488_merged.tif')
     # actin, axon, dendrite = split_tif_image(image)
     # merged, actin_colored, axon_colored, dendrite_colored = get_colored_images(actin, axon, dendrite)
-
